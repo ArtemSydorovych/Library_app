c8c971ac185450fbbe4ca9459b08886df8945617
Artem Sydorovych <artem13444@gmail.com>
2021-01-31 12:12:04 +0100

    added actions

---
+++ my-library/src/store/actions.js
@@ -0,0 +1,9 @@
+export const DOWNLOAD = 'DOWNLOAD'
+export const ADD_BOOK = 'ADD_BOOK'
+export const EDIT_BOOK = 'EDIT_BOOK'
+export const RATE_BOOK = 'RATE_BOOK'
+
+export const setMainState = (type, payload) => ({
+    type,
+    payload
+})
